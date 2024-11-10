1be760831d03f9d62571bfdd727d60577b3dad27
Devarsh-Garala <devarshgarala1307@gmail.com>
2024-11-10 22:24:30 +0530

    Chnged URL

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
-import axios from "axios"
+import axios from "axios";
 const instance = axios.create({
-    baseURL: "http://localhost:8000/api"
-})
-export default instance
+  baseURL: "https://todo-kaushalam-qgyw.onrender.com/api",
+});
+export default instance;
